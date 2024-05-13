707b99b123430da3849521fc168275e50dc7a42f
swastik64 <swastik@me.iitr.ac.in>
2024-05-13 19:15:46 +0530

    updated

--- src/components/singleproduct/single.jsx
+++ src/components/singleproduct/single.jsx
@@ -1,17 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./single.css";
 
 export default function Single() {
-   const {id}= useParams();
-   const [item,setItem]=useState({});
-   useEffect(()=>{
+  const { id } = useParams();
+  const [item, setItem] = useState({});
+  useEffect(() => {
     fetch(`https://dummyjson.com/products/${id}`)
-.then(res => res.json())
-.then(data => setItem(data));
-
-   },[])
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
   return (
-    <>{item&&<div>{item.title}</div>}
+    <>
+      {item && (
+        <div className="single-card">
+          <div
+            style={{
+              width: "100%",
+              paddingInlineStart: "30px",
+              fontSize: "25px",
+              fontWeight: "bold",
+              backgroundColor: "lightcoral",
+            }}
+          >
+            {item.title}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              paddingInlineStart: "30px",
+              fontSize: "25px",
+              fontWeight: "bold",
+              backgroundColor: "yellow",
+            }}
+          >
+            price : {item.price}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              paddingInlineStart: "30px",
+              fontSize: "25px",
+              fontWeight: "bold",
+              backgroundColor: "green",
+            }}
+          >
+            Brand : {item.brand}
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
